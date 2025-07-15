6f7667b9c9a6db6728de804f8af2b078378f7474
J-Hariprakash <j003prakash@gmail.com>
2025-07-15 07:03:20 +0530

    done

---
+++ RotateString.py
@@ -0,0 +1,9 @@
+def rotateString( s, goal):
+        if len(s) == len(goal) and goal in (s+s):
+          return True 
+        else:
+          return False
+Str1 = input("enter a  String one :\n")
+Str2 = input("enter a  String two :\n")
+result = rotateString(Str1,Str2)
+print(result)
